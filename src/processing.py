ca8fb838e053d084547112b3b98c353857bdf4bf
Aidar Yamangulov <a.yamangulov@mail.ru>
2024-10-30 09:05:12 +0500

    добавлены функции и импорт в main.py

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+from typing import List, Dict, Optional
+
+
+def filter_by_state(data: List[Dict], state: Optional[str] = 'EXECUTED') -> List[Dict]:
+    """Фильтрует список словарей по значению ключа "state" """
+    return [item for item in data if item.get('state') == state]
+
+
+def sort_by_date(data: List[Dict], descending: Optional[bool] = True) -> List[Dict]:
+    """Сортирует список словарей по дате"""
+    return sorted(data, key=lambda x: x['date'], reverse=descending)
