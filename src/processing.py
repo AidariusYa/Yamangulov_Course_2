c906bfc0484f285f5d9221ba6bd3f9f83369cc3e
Aidar Yamangulov <a.yamangulov@mail.ru>
2024-11-04 13:04:23 +0500

    исправлены ошибки в функциях

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
-from typing import List, Dict, Optional
+from typing import List, Dict
 
 
-def filter_by_state(data: List[Dict], state: Optional[str] = 'EXECUTED') -> List[Dict]:
+def filter_by_state(data: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     """Фильтрует список словарей по значению ключа "state" """
     return [item for item in data if item.get('state') == state]
 
 
-def sort_by_date(data: List[Dict], descending: Optional[bool] = True) -> List[Dict]:
+def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
     """Сортирует список словарей по дате"""
     return sorted(data, key=lambda x: x['date'], reverse=descending)
